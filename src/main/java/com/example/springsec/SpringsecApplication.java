1f7cf018ab1b71f51f46e09f973d7ff8bce71a62
Hafid ait oubouhou <aitoubouhouhafid@gmail.com>
2020-05-04 23:06:57 +0000

    delete args for security reason + add JCOCO plugin for coverage testing

--- src/main/java/com/example/springsec/SpringsecApplication.java
+++ src/main/java/com/example/springsec/SpringsecApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringsecApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringsecApplication.class, args);
+        SpringApplication.run(SpringsecApplication.class);
     }
 
 }
